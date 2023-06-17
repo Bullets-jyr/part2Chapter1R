89e792a7e07dd68aae2658b7e782f75be06cd665
Bullets-jyr <jyr@bullets.co.kr>
2023-06-17 17:11:30 +0900

    01. 인트로 (개요 및 학습목표) && 02. WebView 사용해보기

--- app/src/main/java/kr/co/bullets/part2chapter1r/MainActivity.kt
+++ app/src/main/java/kr/co/bullets/part2chapter1r/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.co.bullets.part2chapter1r
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.web_view)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://google.com")
     }
 }
